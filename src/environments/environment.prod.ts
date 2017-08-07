bfb5ed4d745a75036d265f96c20f145d2e23cc7c
Sharief <sharief@nbmaclap33.local>
2017-08-07 16:53:33 +0530

    source files for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,16 +5,23 @@ export const environment = {
 */
 
 export const environment = {
-  production: true,
-  api_url : 'http://api.qa1.nbos.in/oauth/token',
+  production: false,
+  api_url : 'http://api.qa1.nbos.in',
   client_id: '13261c94-c778-4976-95f3-774b81591352',
   client_secret: 'web-app-secret',
   grant_type: 'client_credentials',
+  tenant_id: 'TNT:STA-quvnya91',
   theme: 'ng2'
 };
+
 
 export const environmentAdv = {
   production: true,
   api_url: 'https://wavelabs-olx-nonenunciatory-sage.au-syd.mybluemix.net',
-  client_id: '2b84167e-fa95-4b55-b535-e328a0db0341'
-};
+  client_id: '',
+  tenantId: 'TNT:OLX-gz2tq8fy'
+}
+
+export const environmentTodo = {
+  api_url: 'http://php-todo-api.server'
+}
