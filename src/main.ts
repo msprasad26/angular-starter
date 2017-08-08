a53d4722a6bce5e5849a1693286143c58c68d027
Sharief <sharief@nbmaclap33.local>
2017-08-08 15:29:18 +0530

    upldate cli

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+enableProdMode();
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
